de6fe9eb58fc5c9baff3c91cb18e446709d9db14
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-02-08 13:48:02 -0800

    ok

--- R/principal_components_analysis.R
+++ R/principal_components_analysis.R
@@ -56,9 +56,9 @@ plot_pca <- function(pca, draw_lines = list()) {
   for (group in draw_lines) {
     print(group)
     print(group[[1]])
-    for (index in 1:length(group) - 1) {
-      start_treatment = group[[index]]
-      end_treatment = group[[index + 1]]
+    for (i in 1:(length(group) - 1)) {
+      start_treatment = group[[i]]
+      end_treatment = group[[i + 1]]
       print(start_treatment)
       print(coord_by_treat[[start_treatment]])
       start_samples = sapply(
