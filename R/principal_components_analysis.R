4e1b4af7689b2da3e6736ab7f43d04626cf9ab30
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-02-07 16:04:54 -0800

    update

--- R/principal_components_analysis.R
+++ R/principal_components_analysis.R
@@ -44,12 +44,11 @@ coordinates_by_treatment <- function(pca) {
 #' @param pca two-column matrix of principal component coordinates
 #' @export
 plot_pca <- function(pca) {
-  plot(pca[[1]], pca[[2]], col = "white")
+  plot(pca[[1]], pca[[2]])
   coord_by_treat <- coordinates_by_treatment(pca)
   n_treatments <- length(coord_by_treat)
   palette_size <- max(3, n_treatments)
   palette <- brewer.pal(palette_size, "Set1")[c(2, 1, 3:palette_size)]
-  plot(pca, col = "white")
   for (i in n_treatments) {
     points(coord_by_treat[[i]], col = palette[[i]], pch = 19)
   }
