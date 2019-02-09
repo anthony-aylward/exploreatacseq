4651721997e336fdad5d626473fed94304c8fb7d
anthony-aylward <aaylward@eng.ucsd.edu>
2019-02-08 21:56:12 -0800

    test

--- R/explore.R
+++ R/explore.R
@@ -10,8 +10,15 @@
 #'
 #' @param json_file_path path to a JSON file providing data details
 #' @param output_prefix character, a prefix for output files
+#' @param treatment_groups list providing groups of treatments to be compared
+#' @param cores integer, max number of cores to use
 #' @export
-explore <- function(json_file_path, output_prefix, treatment_groups = list()) {
+explore <- function(
+  json_file_path,
+  output_prefix,
+  treatment_groups = list(),
+  cores = 1
+) {
   x <- parse_json(json_file_path)
   peaks <- filter_peaks(
     consensus_peaks(peaks_by_sample(x[["peaks_paths_by_sample"]]))
@@ -27,7 +34,7 @@ explore <- function(json_file_path, output_prefix, treatment_groups = list()) {
       peaks,
       x[["reads_file_paths"]],
       x[["group"]],
-      cores = as.integer(opt[["cores"]])
+      cores = cores
     ),
     batch = x[["batch"]],
     covariates = x[["tss_enrichment"]]
