db0a2e26d2f6dd2d838421de686854c8a6270fa1
anthony-aylward <aaylward@eng.ucsd.edu>
2019-04-08 21:05:19 -0700

    test

--- R/principal_components_analysis.R
+++ R/principal_components_analysis.R
@@ -144,7 +144,9 @@ plot_pca <- function(pca, draw_lines = list(), labels = FALSE) {
   )
   box_colors <- palette[1:n_treatments][order(sapply(coord_by_treat, function(x) median(x[,1])))]
   by_median <- reorder(grp, pc[[1]], median)
-  boxplot(pc[[1]] ~ by_median, horizontal = TRUE, las = 1, col =  box_colors)
+  boxplot(pc[[1]] ~ by_median, horizontal = TRUE, las = 1, col =  box_colors, axes = FALSE)
+  axis(1)
+  axis(4)
   boxplot(pc[[2]] ~ by_median, las = 2, col =  box_colors)
   plot(0:1, 0:1, col = "white", xaxt = "n", yaxt = "n", bty = "n", ann = FALSE)
   legend(
