1294377dbd084dd829d91fa1c72a97b3db4ae04f
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-06-13 14:37:11 -0700

    update

--- R/principal_components_analysis.R
+++ R/principal_components_analysis.R
@@ -23,7 +23,7 @@ exploreatacseq_color_palette <- function(order = "categorical") {
   if (order == "categorical") {
     brewer.pal(9, "Set1")[c(2, 1, 3:5, 7:9, 6)]
   } else if (order == "sequential") {
-    brewer.pal(8, "Set1")[c(8, 4, 2, 3, 5, 1)]
+    rev(brewer.pal(6, "RdYlBu"))
   }
 }
 
