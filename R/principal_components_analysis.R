29b1d6bc409399f89401b7237f1e7bf5c5d1e2a9
Anthony Aylward <aaylward@eng.ucsd.edu>
2019-09-23 13:49:30 -0700

    tryt his

--- R/principal_components_analysis.R
+++ R/principal_components_analysis.R
@@ -98,7 +98,7 @@ plot_pca <- function(
     widths = c(1, 2),
     heights = c(2, 1)
   )
-  par(mai=c(0.6, 0.6, 0.1, 0.1))
+  par(mai=c(0.1, 0.1, 0.1, 0.1), oma=c(0.7, 0.7, 0.1, 0.1))
   
   plot(
     pca[["rotation"]][,1],
