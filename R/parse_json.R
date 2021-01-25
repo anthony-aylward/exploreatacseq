1aed09a23a493d0020b0de92f12ba30c505a890b
anthony-aylward <aaylward@eng.ucsd.edu>
2021-01-25 13:54:37 -0800

    test replacing sapply with lapply

--- R/parse_json.R
+++ R/parse_json.R
@@ -18,7 +18,7 @@
 extract_peaks_paths_by_sample <- function(input_list) {
   lapply(
     input_list,
-    function(treatment_list) sapply(
+    function(treatment_list) lapply(
       treatment_list,
       function(treatment) {
         if (sort(names(treatment)) == c("peaks", "reads", "tssenrich")) { 
