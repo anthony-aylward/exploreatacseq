626ffd4abf464a323c62fd635281de94dace400a
anthony-aylward <aaylward@eng.ucsd.edu>
2021-02-06 15:17:06 -0800

    start writing vignette

---
+++ R/SCATEData_accession_sample.R
@@ -0,0 +1,19 @@
+#' Table mapping SCATEData accession numbers to sample type
+#'
+#' The bioconductor package SCATEData includes 20 BAM files containing
+#' single-cell ATAC-seq data. Each dataset is named by its NCBI Sequence Read
+#' Archive accession number, "SRR1779746.bam". This table lists the sample type
+#' of each dataset.
+#'
+#' @docType data
+#'
+#' @usage data(SCATEData_accession_sample)
+#'
+#' @format A data frame with two columns: "accession" and "sample".
+#'
+#' @keywords datasets
+#'
+#' @references Zhicheng Ji, Weiqiang Zhou, Wenpin Hou, Hongkai Ji (2020) Genome Biol 21, 161
+#' (\href{https://genomebiology.biomedcentral.com/articles/10.1186/s13059-020-02075-3}{Genome Biol})
+#'
+#' @source \href{https://www.ncbi.nlm.nih.gov/sra/}{NCBI Sequence Read Archive}
