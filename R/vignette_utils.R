d981ec0830781749b5d10488ca911b3fc8400fcc
anthony-aylward <aaylward@eng.ucsd.edu>
2021-02-07 18:51:51 -0800

    map_paths_to_samples()

--- R/vignette_utils.R
+++ R/vignette_utils.R
@@ -1,3 +1,4 @@
+#' @importFrom stats setNames
 #' @importFrom stringr str_replace
 
 #' @title locate SCATEData files
@@ -15,4 +16,21 @@ locate_SCATEData_files <- function() {
     bam_list <- bam_list[grepl(".bam$", bam_list)]
     names(bam_list) <- str_replace(basename(bam_list), ".bam$", "")
     bam_list
+}
+
+#' @title map SCATEData file paths to sample types
+#'
+#' @description map SCATEData file paths to sample types
+#'
+#' @return list mapping file paths to sample types, sorted by sample types
+#' @export
+map_paths_to_samples <- function() {
+    data(SCATEData_sample)
+    SCATEData_path <- locate_SCATEData_files()
+    samples <- vapply(
+        names(SCATEData_path),
+        function(name) SCATEData_sample[[name]],
+        character(length = 1)
+    )
+    sort(setNames(samples, unname(SCATEData_path)))
 }
