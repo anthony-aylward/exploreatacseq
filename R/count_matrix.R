7575043fd255e89f8f63be840e1d9a9b90756356
anthony-aylward <aaylward@eng.ucsd.edu>
2021-04-04 01:47:07 -0700

    clean up

--- R/count_matrix.R
+++ R/count_matrix.R
@@ -36,7 +36,6 @@ get_raw_counts <- function(peaks, reads_file_paths, cores = 1) {
     )
     raw_counts <- assays(summarized_experiment)[["counts"]]
     rownames(raw_counts) <- names(peaks)
-    print(head(raw_counts))
     raw_counts
 }
 
